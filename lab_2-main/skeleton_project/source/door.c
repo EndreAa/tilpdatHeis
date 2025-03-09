e4a224b30331d62e2ef54de2080cb9be16db4148
MaiSkjelby <mai.skjelby@gmail.com>
2025-03-09 16:52:25 +0100

    prøver noe nytt med timer

--- lab_2-main/skeleton_project/source/door.c
+++ lab_2-main/skeleton_project/source/door.c
@@ -1,18 +1,10 @@
 #include "door.h"
-#include <time.h>
+#include <unistd.h>
 
 const int DOOR_OPEN_TIME_MS = 3000;
 const int TIMER_CHECK_INTERVAL_MS = 100;
 
-static long door_timer;
 static int door_is_open = 0;
-static int door_timer_active = 0;
-
-static long get_current_time_ms() {
-    struct timespec now;
-    timespec_get(&now, TIME_UTC);
-    return now.tv_sec * 1000L + now.tv_nsec / 1000000L;
-}
 
 int door_open(ElevatorSM *sm) {
     int current_floor = elevio_floorSensor();
@@ -37,83 +29,37 @@ int door_close(ElevatorSM *sm) {
            elevio_obstruction(), was_obstructed);
     
     if (elevio_obstruction() == 1) {
-        printf("DEBUG [DOOR]: Door obstructed, resetting timer\n");
-        was_obstructed = 1;
-        door_timer = get_current_time_ms();
-        return 0;
-    }
-
-    if (was_obstructed) {
-        printf("DEBUG [DOOR]: Previously obstructed, resetting timer\n");
-        door_timer = get_current_time_ms(); 
-        was_obstructed = 0;   
+        printf("DEBUG [DOOR]: Door obstructed, cannot close\n");
         return 0;
     }
     
     elevio_doorOpenLamp(0);
     door_is_open = 0;
-    door_timer_active = 0;
     printf("DEBUG [DOOR]: Door closed successfully\n");
     
     return 1;
 }
 
-void door_timer_start(ElevatorSM *sm) {
-    door_timer = get_current_time_ms();
-    door_timer_active = 1;
-    printf("DEBUG [DOOR]: Timer started at %ld ms\n", door_timer);
-}
-
-int door_timer_expired(ElevatorSM *sm) {
-    if (!door_timer_active) {
-        printf("DEBUG [DOOR]: Timer not active\n");
-        return 0;
-    }
-    
-    long current_time = get_current_time_ms();
-    long elapsed_ms = current_time - door_timer;
-    
-    printf("DEBUG [DOOR]: Timer check - Elapsed: %ld ms of %d ms\n", elapsed_ms, DOOR_OPEN_TIME_MS);
-    
-    if (elapsed_ms >= DOOR_OPEN_TIME_MS) {
-        printf("DEBUG [DOOR]: Timer EXPIRED\n");
-        return 1;
-    }
-    
-    return 0;
-}
-
 int door_deliver_to_floor(ElevatorSM *sm) {
-    printf("DEBUG [DOOR]: door_deliver_to_floor - door_is_open: %d, timer_active: %d\n", door_is_open, door_timer_active);
+    printf("DEBUG [DOOR]: door_deliver_to_floor - door_is_open: %d\n", door_is_open);
 
     if (!door_is_open) {
         printf("DEBUG [DOOR]: Door not open, attempting to open\n");
-        if (door_open(sm)) {
-            printf("DEBUG [DOOR]: Door opened, starting timer\n");
-            door_timer_start(sm);
-        } else {
+        if (!door_open(sm)) {
             printf("DEBUG [DOOR]: Failed to open door\n");
+            return 0;
         }
     }
     
-    if (elevio_stopButton() == 1) {
-        printf("DEBUG [DOOR]: Stop button pressed, resetting timer\n");
-        door_timer_start(sm);
-    }
-
-    if (door_timer_expired(sm)) {
-        printf("DEBUG [DOOR]: Timer expired, attempting to close door\n");
-        if (door_close(sm)) {
-            printf("DEBUG [DOOR]: Door closed, delivery complete\n");
-            return 1;
-        } else {
-            printf("DEBUG [DOOR]: Could not close door, delivery continuing...\n");
-        }
+    sleep(3);
+    
+    printf("DEBUG [DOOR]: Attempting to close door after sleep(3)\n");
+    if (door_close(sm)) {
+        printf("DEBUG [DOOR]: Door closed, delivery complete\n");
+        return 1;
     } else {
-        printf("DEBUG [DOOR]: Waiting for timer to expire\n");
+        printf("DEBUG [DOOR]: Could not close door, delivery continuing...\n");
+        return 0;
     }
-
-    return 0;
 }
-
 
