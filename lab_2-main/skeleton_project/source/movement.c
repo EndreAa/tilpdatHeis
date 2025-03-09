096abd8fd7b4389fc33ae4fdabe0a98ea185f91c
MaiSkjelby <mai.skjelby@gmail.com>
2025-03-09 13:02:33 +0100

    Update movement.c

--- lab_2-main/skeleton_project/source/movement.c
+++ lab_2-main/skeleton_project/source/movement.c
@@ -4,6 +4,7 @@
 ElevatorState movement_get_to_floor(ElevatorSM *sm){
     if (sm->target_floor == elevio_floorSensor()){
         sm->elevator_direction = 0;
+        elevio_motorDirection(sm->elevator_direction);
         return state_deliver;
     } else {
         elevio_motorDirection(sm->elevator_direction);
