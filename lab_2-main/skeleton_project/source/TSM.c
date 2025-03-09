5bcbb3c32d534e3c06317c575ed88451d75fdae6
MaiSkjelby <mai.skjelby@gmail.com>
2025-03-09 16:25:44 +0100

    fikset debugging tsm

--- lab_2-main/skeleton_project/source/TSM.c
+++ lab_2-main/skeleton_project/source/TSM.c
@@ -1,6 +1,5 @@
 #include "TSM.h"
 
-
 ElevatorState TSM_state_stop(ElevatorSM *sm, StateEvent event)
 {
     switch (event) {
@@ -25,41 +24,35 @@ ElevatorState TSM_state_move(ElevatorSM *sm, StateEvent event)
 {
     switch (event) {
     case event_enter:
-        printf("DEBUG [TSM %s]: Entering move state, target floor: %d\n", 
-               get_timestamp(), sm->target_floor);
+        printf("Entering move state, target floor: %d\n", sm->target_floor);
         
         if (sm->target_floor == NO_ORDER) {
-            printf("DEBUG [TSM %s]: No target floor in move state! Returning to still\n", 
-                   get_timestamp());
+            printf("No target floor in move state! Returning to still\n");
             return state_still;
         }
         
         sm->elevator_direction = movement_choose_direction(sm);
-        printf("DEBUG [TSM %s]: Chosen direction: %d\n", 
-               get_timestamp(), sm->elevator_direction);
+        printf("Chosen direction: %d\n", sm->elevator_direction);
         
         elevio_motorDirection(sm->elevator_direction); 
         break;
         
     case event_execute:
-        printf("DEBUG [TSM %s]: Execute move state, target: %d, current: %d\n", 
-               get_timestamp(), sm->target_floor, elevio_floorSensor());
+        printf("Execute move state, target: %d, current: %d\n", sm->target_floor, elevio_floorSensor());
 
         if (sm->target_floor == NO_ORDER) {
-            printf("DEBUG [TSM %s]: No target floor in move state! Stopping motor\n", 
-                   get_timestamp());
+            printf("No target floor in move state! Stopping motor\n");
             elevio_motorDirection(DIRN_STOP);
             return state_still;
         }
         
         ElevatorState next_state = movement_get_to_floor(sm);
-        printf("DEBUG [TSM %s]: movement_get_to_floor returned state: %d\n", 
-               get_timestamp(), next_state);
+        printf("movement_get_to_floor returned state: %d\n", next_state);
         
         return next_state;
         
     case event_exit:
-        printf("DEBUG [TSM %s]: Exiting move state\n", get_timestamp());
+        printf("Exiting move state\n");
         break;
     }
     return state_move;
@@ -69,46 +62,39 @@ ElevatorState TSM_state_deliver(ElevatorSM *sm, StateEvent event)
 {
     switch (event) {
     case event_enter:
-        printf("DEBUG [TSM %s]: Entering deliver state for floor %d\n", 
-               get_timestamp(), sm->target_floor);
+        printf("Entering deliver state for floor %d\n", sm->target_floor);
         break;
         
     case event_execute:
-        printf("DEBUG [TSM %s]: Execute deliver state for floor %d, queue count: %d\n", 
-               get_timestamp(), sm->target_floor, sm->queue.queue_count);
+        printf("Execute deliver state for floor %d, queue count: %d\n", sm->target_floor, sm->queue.queue_count);
         
         int door_result = door_deliver_to_floor(sm);
-        printf("DEBUG [TSM %s]: door_deliver_to_floor returned: %d\n", 
-               get_timestamp(), door_result);
+        printf("door_deliver_to_floor returned: %d\n", door_result);
         
         if (door_result == 0) {
-            printf("DEBUG [TSM %s]: Delivery still in progress\n", get_timestamp());
+            printf("Delivery still in progress\n");
             return state_deliver;
         } else {
-            printf("DEBUG [TSM %s]: Delivery completed for floor %d\n", 
-                   get_timestamp(), sm->target_floor);
+            printf("Delivery completed for floor %d\n", sm->target_floor);
             
             int remove_result = queue_remove(&(sm->queue), sm->target_floor);
-            printf("DEBUG [TSM %s]: queue_remove returned: %d\n", 
-                   get_timestamp(), remove_result);
+            printf("queue_remove returned: %d\n", remove_result);
             
             lights_turn_off(sm->target_floor);
             
             if (sm->queue.queue_count == 0) {
-                printf("DEBUG [TSM %s]: Queue empty after delivery, moving to still state\n", 
-                       get_timestamp());
+                printf("Queue empty after delivery, moving to still state\n");
                 sm->target_floor = NO_ORDER;
                 return state_still;
             } else {
                 sm->target_floor = sm->queue.queue_list[0];
-                printf("DEBUG [TSM %s]: Next target floor: %d\n", 
-                       get_timestamp(), sm->target_floor);
+                printf("Next target floor: %d\n", sm->target_floor);
                 return state_move;
             }
         }
         
     case event_exit:
-        printf("DEBUG [TSM %s]: Exiting deliver state\n", get_timestamp());
+        printf("Exiting deliver state\n");
         break;
     }
     return state_deliver;
@@ -118,27 +104,24 @@ ElevatorState TSM_state_still(ElevatorSM *sm, StateEvent event)
 {
     switch (event) {
     case event_enter:
-        printf("DEBUG [TSM %s]: Entering still state\n", get_timestamp());
+        printf("Entering still state\n");
         elevio_motorDirection(DIRN_STOP);
         sm->elevator_direction = 0;
         break;
         
     case event_execute:
-        printf("DEBUG [TSM %s]: Execute still state, target floor: %d, queue count: %d\n", 
-               get_timestamp(), sm->target_floor, sm->queue.queue_count);
+        printf("Execute still state, target floor: %d, queue count: %d\n", sm->target_floor, sm->queue.queue_count);
         
         if (sm->queue.queue_count > 0) {
-            printf("DEBUG [TSM %s]: Orders in queue, moving to move state\n", 
-                   get_timestamp());
+            printf("Orders in queue, moving to move state\n");
             return state_move;
         } else {
-            printf("DEBUG [TSM %s]: No orders, remaining in still state\n", 
-                   get_timestamp());
+            printf("No orders, remaining in still state\n");
             return state_still;
         }
         
     case event_exit:
-        printf("DEBUG [TSM %s]: Exiting still state\n", get_timestamp());
+        printf("Exiting still state\n");
         break;
     }
     return state_still;
@@ -164,7 +147,6 @@ void TSM_call_exit(ElevatorSM *sm, ElevatorState state)
     }
 }
 
-
 void TSM_call_enter(ElevatorSM *sm, ElevatorState state)
 {
     switch (state) {
@@ -185,27 +167,24 @@ void TSM_call_enter(ElevatorSM *sm, ElevatorState state)
     }
 }
 
-
 ElevatorState TSM_update(ElevatorSM *sm){
-    printf("\nDEBUG [TSM %s]: === Begin TSM Update ===\n", get_timestamp());
-    printf("DEBUG [TSM %s]: Current state: %d, Target floor: %d, Last floor: %d, Direction: %d\n",
-           get_timestamp(), sm->current_state, sm->target_floor, 
-           sm->last_current_floor, sm->elevator_direction);
-    printf("DEBUG [TSM %s]: Door status - Is open: %d\n", get_timestamp(), door_is_open);
+    printf("\n=== Begin TSM Update ===\n");
+    printf("Current state: %d, Target floor: %d, Last floor: %d, Direction: %d\n",
+           sm->current_state, sm->target_floor, sm->last_current_floor, sm->elevator_direction);
+    printf("Door status - Is open: %d\n", door_is_open);
     
     int sensor_floor = elevio_floorSensor();
-    printf("DEBUG [TSM %s]: Current floor sensor: %d\n", get_timestamp(), sensor_floor);
+    printf("Current floor sensor: %d\n", sensor_floor);
     
     if (sensor_floor != -1) {
         sm->last_current_floor = sensor_floor;
     }
     lights_last_floor_lamp(sm);
     
-    printf("DEBUG [TSM %s]: Stop button: %d\n", get_timestamp(), elevio_stopButton());
+    printf("Stop button: %d\n", elevio_stopButton());
     if (elevio_stopButton() == 1) {
         if (sm->current_state != state_stop) {
-            printf("DEBUG [TSM %s]: Stop button pressed - transitioning to stop state\n", 
-                   get_timestamp());
+            printf("Stop button pressed - transitioning to stop state\n");
             TSM_call_exit(sm, sm->current_state);
             TSM_call_enter(sm, state_stop);
             sm->current_state = state_stop;
@@ -213,8 +192,7 @@ ElevatorState TSM_update(ElevatorSM *sm){
         return state_stop;
     } else {
         if (sm->current_state == state_stop) {
-            printf("DEBUG [TSM %s]: Stop button released - transitioning to still state\n", 
-                   get_timestamp());
+            printf("Stop button released - transitioning to still state\n");
             TSM_call_exit(sm, state_stop);
             TSM_call_enter(sm, state_still);
             sm->current_state = state_still;
@@ -224,7 +202,7 @@ ElevatorState TSM_update(ElevatorSM *sm){
     sensors_update(&(sm->sensors));
     orders_register_order(sm);
     
-    printf("DEBUG [TSM %s]: Queue status: ", get_timestamp());
+    printf("Queue status: ");
     queue_print(&(sm->queue));
     
     int old_target = sm->target_floor;
@@ -235,14 +213,13 @@ ElevatorState TSM_update(ElevatorSM *sm){
     }
     
     if (old_target != sm->target_floor) {
-        printf("DEBUG [TSM %s]: Target floor updated: %d -> %d\n", 
-               get_timestamp(), old_target, sm->target_floor);
+        printf("Target floor updated: %d -> %d\n", old_target, sm->target_floor);
     }
     
     ElevatorState old_state = sm->current_state;
     ElevatorState next_state = old_state;
     
-    printf("DEBUG [TSM %s]: Processing state: %d\n", get_timestamp(), old_state);
+    printf("Processing state: %d\n", old_state);
     
     switch (old_state) {
     case state_still:
@@ -259,27 +236,14 @@ ElevatorState TSM_update(ElevatorSM *sm){
     }
     
     if (next_state != old_state) {
-        printf("DEBUG [TSM %s]: State transition: %d -> %d\n", 
-               get_timestamp(), old_state, next_state);
+        printf("State transition: %d -> %d\n", old_state, next_state);
         TSM_call_exit(sm, old_state);
         TSM_call_enter(sm, next_state);
         sm->current_state = next_state;
     } else {
-        printf("DEBUG [TSM %s]: Remaining in state: %d\n", get_timestamp(), old_state);
+        printf("Remaining in state: %d\n", old_state);
     }
     
-    printf("DEBUG [TSM %s]: === End TSM Update ===\n\n", get_timestamp());
+    printf("=== End TSM Update ===\n\n");
     return next_state;
-}
-
-static char* get_timestamp() {
-    static char buffer[26];
-    time_t timer;
-    struct tm* tm_info;
-    
-    time(&timer);
-    tm_info = localtime(&timer);
-    
-    strftime(buffer, 26, "%H:%M:%S", tm_info);
-    return buffer;
 }
