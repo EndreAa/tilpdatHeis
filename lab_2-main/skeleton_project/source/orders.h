75da1ec7b1c583342653467e7bf2ecccddd32d0b
MaiSkjelby <mai.skjelby@gmail.com>
2025-03-12 12:04:04 +0100

    Update orders.h

--- lab_2-main/skeleton_project/source/orders.h
+++ lab_2-main/skeleton_project/source/orders.h
@@ -6,4 +6,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+/**
+ * @brief Iterates through the nested vector contaning the order button information and registers
+ * orders if they are placed.
+ *
+ *
+ * @param *sm A pointer to elevator state machine. Needed to give access to sensor information.
+ */ 
 void orders_register_order(ElevatorSM *sm);
