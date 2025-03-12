0af679e46db96d098bbf282826fa9381a5a54158
MaiSkjelby <mai.skjelby@gmail.com>
2025-03-12 12:12:05 +0100

    Update start.h

--- lab_2-main/skeleton_project/source/start.h
+++ lab_2-main/skeleton_project/source/start.h
@@ -8,7 +8,19 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+/**
+ * @brief Ensures the elevator enters a defined state before
+ * the state machine is initialised. The elevator moves down until
+ * it reaches a floor.
+ *
+ * @return bool Returns true when the elevator is in a defined state 
+ * (at a floor).
+ */
 bool start_find_floor();
 
+/**
+ * @brief Instansiates the elevator state machine and updates the 
+ * state machine in an endless while loop.
+ */
 void start_init();
 
